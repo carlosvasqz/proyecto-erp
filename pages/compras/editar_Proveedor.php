f726352d9c34a25386233227839105be774d02bd
CVz <carlosdavidvasquez7@gmail.com>
2018-07-03 18:17:38 -0600

    Revirtiendo los cambios de los modulos de compras

--- pages/compras/editar_Proveedor.php
+++ pages/compras/editar_Proveedor.php
@@ -1,37 +1,28 @@
-<style type= "text/css">
-#incorrecto{
-	color:red;
-	font-weight: bold;
-}
-#correcto{
-	color:green;
-	font-weight: bold;
-}
-</style>
 <?php
-include ('../../inc/conexion.php');
-$Id=$_GET['Id'];
-$nombre=$_GET['nombre'];
-$rtn=$_GET['rtn'];
-$direccion=$_GET['direccion'];
-$telefono=$_GET['telefono'];
-$correo=$_GET['correo'];
-$estado=$_GET['estado'];
-$sql=mysqli_query($db,"UPDATE proveedores SET  Nombre_Proveedor='$nombre',RTN_Proveedor='$rtn', Direccion='$direccion', Telefono='$telefono', Correo_Electronico='$correo', Estado='$estado' WHERE Id_Proveedor='$Id'") or die(mysqli_error());
+	//session_start();
+	//if (isset($_SESSION['username'])&&($_SESSION['type'])) { 
 
-header("location:proveedores_editar.php?Id=$Id");
-echo '<span id="correcto">Modificado Con Exito.</span>';
+        include ('../../inc/conexion.php');
+        include ('../../inc/util.php');
+$codigo= $_POST['codigo'];
+$nombre= $_POST['nombre'];
+$rtn= $_POST['rtn'];
+$direccion= $_POST['direccion'];
+$telefono=$_POST['telefono'];
+$correo= $_POST['correo'];
+$estado= $_POST['estado'];
+
+$queryVerificar = mysqli_query($db, "SELECT COUNT(*) as Existe FROM proveedores WHERE Id_Proveedor = '$codigo' AND RTN_Proveedor = '$rtn'") or die (mysqli_error());
+    
+	$rowExiste=mysqli_fetch_array($queryVerificar);
+	if($rowExiste['Existe']==0){
+		#echo 'No existe';
+	}
+	if ($rowExiste['Existe']==1) {
+$sql=mysqli_query($db,"UPDATE proveedores SET  Nombre_Proveedor='$nombre',RTN_Proveedor='$rtn', Direccion='$direccion', Telefono='$telefono', Correo_Electronico='$correo', Estado= $estado  WHERE Id_Proveedor='$codigo' AND RTN_Proveedor='$rtn'") or die(mysqli_error());	echo 'Guardado';
+        }
+            
+    // } else {
+    //     header('location: page_denegado.php');
+    // }
 ?>
-<script type="text/javascript">
-	$('input[name=nombre]').val("");
-	$('input[name=rtn]').val("");
-	$('input[name=direccion]').val("");
-	$('input[name=telefono]').val("");
-	$('input[name=correo]').val("");
-	$('input[name=estado]').val("");
-  
-	$("#correcto").fadeOut(3000);
-	</script>
-	<?php
-
-	?>
