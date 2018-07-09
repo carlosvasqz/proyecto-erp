24c1b18adf3bfa30ea868dae4118b0929840944e
CVz <carlosdavidvasquez7@gmail.com>
2018-07-08 22:19:39 -0600

    Cambio de lugar

--- lockscreen.php
+++ lockscreen.php
@@ -7,17 +7,17 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="../../bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="../../bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="../../bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="../bower_components/Ionicons/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
   <!-- Material Design -->
-  <link rel="stylesheet" href="../../dist/css/bootstrap-material-design.min.css">
-  <link rel="stylesheet" href="../../dist/css/ripples.min.css">
-  <link rel="stylesheet" href="../../dist/css/MaterialAdminLTE.min.css">
+  <link rel="stylesheet" href="../dist/css/bootstrap-material-design.min.css">
+  <link rel="stylesheet" href="../dist/css/ripples.min.css">
+  <link rel="stylesheet" href="../dist/css/MaterialAdminLTE.min.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -33,7 +33,7 @@
 <!-- Automatic element centering -->
 <div class="lockscreen-wrapper">
   <div class="lockscreen-logo">
-    <a href="../../index2.php">Material<b>Admin</b>LTE</a>
+    <a href="../index2.php">Material<b>Admin</b>LTE</a>
   </div>
   <!-- User name -->
   <div class="lockscreen-name">John Doe</div>
