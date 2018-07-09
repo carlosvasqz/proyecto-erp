79c1a4f3eddd7f615167150bba6b365db13aebac
CVz <carlosdavidvasquez7@gmail.com>
2018-07-08 22:23:37 -0600

    Cambio de nombre y modificaciones de acceso al menu

--- inc/menus.php
+++ inc/menus.php
@@ -1,16 +1,165 @@
 <?php
     //session_start();
-    function menuRoot() {
-?>
 
-<ul class="sidebar-menu" data-widget="tree">
+    function cargarAdmin(){
+      $admin = array(
+        'empleados' => '',
+        'empleados_registrar' => '',
+        'empleados_editar' => '',
+        'tipos_usuarios' => '',
+        'tipos_usuarios_registrar' => '',
+        'tipos_usuarios_editar' => '',
+        'usuarios' => ''
+      );
+      return $admin;
+    }
+
+    function cargarCompras(){
+      $compras = array(
+        'cotizaciones_compra' => '',
+        'ordenes_compra' => '',
+        'proveedores' => '',
+        'proveedores_registrar' => '',
+        'proveedores_editar' => ''
+      );
+      return $compras;
+    }
+
+    function cargarConf(){
+      $conf = array(
+        'perfil' => ''
+      );
+      return $conf;
+    }
+
+    function cargarCont(){
+      $cont = array(
+        'cierres_diarios' => '',
+        'reportes' => ''
+      );
+      return $cont;
+    }
+
+    function cargarInv(){
+      $inv = array(
+        'articulos' => '',
+        'categorias' => '',
+        'categorias_registrar' => '',
+        'categorias_editar' => '',
+        'conversiones' => ''
+      );
+      return $inv;
+    }
+
+    function cargarVentas(){
+      $ventas = array(
+        'clientes' => '',
+        'clientes_registrar' => '',
+        'clientes_editar' => '',
+        'cotizaciones_venta' => '',
+        'registrar_cliente' => '',
+        'registro_ventas' => ''
+      );
+      return $ventas;
+    }
+
+    function cargarClavesTree(){
+      // valor a contener "active "
+      $modulos = array(
+        // administracion
+        'administracion' => '',
+        // compras
+        'compras' => '',
+        // configuraciones
+        'configuraciones' => '',
+        // contabilidad
+        'contabilidad' => '',
+        // inventario
+        'inventario' => '',
+        // ventas
+        'ventas' => ''
+      );
+      return $tree;
+    }
+    
+    function cargarClavesLi(){
+      // valor a contener " class = 'active'"
+      $li = array(
+        // principal
+        'index' => '',
+        // administracion
+        'empleados' => '',
+        'empleados_registrar' => '',
+        'empleados_editar' => '',
+        'tipos_usuarios' => '',
+        'tipos_usuarios_registrar' => '',
+        'tipos_usuarios_editar' => '',
+        'usuarios' => '',
+        // compras
+        'cotizaciones_compra' => '',
+        'ordenes_compra' => '',
+        'proveedores' => '',
+        'proveedores_registrar' => '',
+        'proveedores_editar' => '',
+        // configuraciones
+        'perfil' => '',
+        // contabilidad
+        'cierres_diarios' => '',
+        'reportes' => '',
+        // inventario
+        'articulos' => '',
+        'categorias' => '',
+        'categorias_registrar' => '',
+        'categorias_editar' => '',
+        'conversiones' => '',
+        // ventas
+        'clientes' => '',
+        'clientes_registrar' => '',
+        'clientes_editar' => '',
+        'cotizaciones_venta' => '',
+        'registrar_cliente' => '',
+        'registro_ventas' => ''
+      );
+      return $li;
+    }
+
+    function menuRoot($cd, $fileName) {
+      $tree = cargarClavesTree();
+      $admin = cargarAdmin();
+      $compras = cargarCompras();
+      $conf = cargarConf();
+      $cont = cargarCont();
+      $inv = cargarInv();
+      $ventas = cargarVentas();
+      $li = cargarClavesLi();
+      $li[$fileName] = ' class = "active"';
+      if (array_key_exists($fileName, $admin)) {
+        $modulos['administracion'] = "active ";
+      }
+      if (array_key_exists($fileName, $compras)) {
+        $modulos['compras'] = "active ";
+      }
+      if (array_key_exists($fileName, $conf)) {
+        $modulos['configuraciones'] = "active ";
+      }
+      if (array_key_exists($fileName, $cont)) {
+        $modulos['contabilidad'] = "active ";
+      }
+      if (array_key_exists($fileName, $inv)) {
+        $modulos['inventario'] = "active ";
+      }
+      if (array_key_exists($fileName, $ventas)) {
+        $modulos['ventas'] = "active ";
+      }
+      echo '
+      <ul class="sidebar-menu" data-widget="tree">
         <li class="header">NAVEGACION PRINCIPAL</li>
-        <li class="active">
-          <a href="index.php">
+        <li'.$li['index'].'>
+          <a href="'.$cd.'index.php">
             <i class="fa fa-dashboard"></i> <span>Panel Principal</span>
           </a>
         </li>
-        <li class="treeview">
+        <li class="'.$modulos['administracion'].'treeview">
           <a href="#">
             <i class="fa fa-share"></i> <span>Administración</span>
             <span class="pull-right-container">
@@ -18,9 +167,9 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="pages/administracion/empleados.php"><i class="fa fa-circle-o"></i> Empleados</a></li> 
-            <li><a href="pages/administracion/usuarios.php"><i class="fa fa-circle-o"></i> Usuarios</a></li>
-            <li><a href="pages/administracion/tipos_usuarios.php"><i class="fa fa-circle-o"></i> Tipos de Usuarios</a></li>
+            <li'.$li['empleados'].'><a href="'.$cd.'pages/administracion/empleados.php"><i class="fa fa-circle-o"></i> Empleados</a></li> 
+            <li'.$li['usuarios'].'><a href="'.$cd.'pages/administracion/usuarios.php"><i class="fa fa-circle-o"></i> Usuarios</a></li>
+            <li'.$li['tipos_usuarios'].'><a href="'.$cd.'pages/administracion/tipos_usuarios.php"><i class="fa fa-circle-o"></i> Tipos de Usuarios</a></li>
             <!-- <li class="treeview">
               <a href="#"><i class="fa fa-circle-o"></i> Nivel Uno
                 <span class="pull-right-container">
@@ -44,7 +193,7 @@
             </li> -->
           </ul>
         </li>
-        <li class="treeview">
+        <li class="'.$modulos['compras'].'treeview">
           <a href="#">
             <i class="fa fa-share"></i> <span>Compras</span>
             <span class="pull-right-container">
@@ -52,10 +201,10 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="pages/compras/registro_compras.php"><i class="fa fa-circle-o"></i> Registro de Compras</a></li> 
-            <li><a href="pages/compras/proveedores.php"><i class="fa fa-circle-o"></i> Proveedores</a></li>
-            <li><a href="pages/compras/cotizaciones_compra.php"><i class="fa fa-circle-o"></i> Cotizaciones de Compra</a></li>
-            <li><a href="pages/compras/ordenes_compra.php"><i class="fa fa-circle-o"></i> Ordenes de Compra</a></li>
+            <li'.$li['registro_compras'].'><a href="'.$cd.'pages/compras/registro_compras.php"><i class="fa fa-circle-o"></i> Registro de Compras</a></li> 
+            <li'.$li['proveedores'].'><a href="'.$cd.'pages/compras/proveedores.php"><i class="fa fa-circle-o"></i> Proveedores</a></li>
+            <li'.$li['cotizaciones_compra'].'><a href="'.$cd.'pages/compras/cotizaciones_compra.php"><i class="fa fa-circle-o"></i> Cotizaciones de Compra</a></li>
+            <li'.$li['ordenes_compra'].'><a href="'.$cd.'pages/compras/ordenes_compra.php"><i class="fa fa-circle-o"></i> Ordenes de Compra</a></li>
             <!-- <li class="treeview">
               <a href="#"><i class="fa fa-circle-o"></i> Nivel Uno
                 <span class="pull-right-container">
@@ -79,7 +228,7 @@
             </li> -->
           </ul>
         </li>
-        <li class="treeview">
+        <li class="'.$modulos['ventas'].'treeview">
           <a href="#">
             <i class="fa fa-share"></i> <span>Ventas</span>
             <span class="pull-right-container">
@@ -87,9 +236,9 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="pages/ventas/registro_ventas.php"><i class="fa fa-circle-o"></i> Registro de Ventas</a></li> 
-            <li><a href="pages/ventas/clientes.php"><i class="fa fa-circle-o"></i> Clientes</a></li>
-            <li><a href="pages/ventas/cotizaciones_venta.php"><i class="fa fa-circle-o"></i> Cotizaciones de Venta</a></li>
+            <li'.$li['registro_ventas'].'><a href="'.$cd.'pages/ventas/registro_ventas.php"><i class="fa fa-circle-o"></i> Registro de Ventas</a></li> 
+            <li'.$li['clientes'].'><a href="'.$cd.'pages/ventas/clientes.php"><i class="fa fa-circle-o"></i> Clientes</a></li>
+            <li'.$li['cotizaciones_venta'].'><a href="'.$cd.'pages/ventas/cotizaciones_venta.php"><i class="fa fa-circle-o"></i> Cotizaciones de Venta</a></li>
             <!-- <li class="treeview">
               <a href="#"><i class="fa fa-circle-o"></i> Nivel Uno
                 <span class="pull-right-container">
@@ -113,7 +262,7 @@
             </li> -->
           </ul>
         </li>
-        <li class="treeview">
+        <li class="'.$modulos['inventario'].'treeview">
           <a href="#">
             <i class="fa fa-share"></i> <span>Inventario</span>
             <span class="pull-right-container">
@@ -121,9 +270,9 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="pages/inventario/categorias.php"><i class="fa fa-circle-o"></i> Categorías</a></li> 
-            <li><a href="pages/inventario/articulos.php"><i class="fa fa-circle-o"></i> Artículos</a></li>
-            <li><a href="pages/inventario/conversiones.php"><i class="fa fa-circle-o"></i> Conversiones</a></li>
+            <li'.$li['categorias'].'><a href="'.$cd.'pages/inventario/categorias.php"><i class="fa fa-circle-o"></i> Categorías</a></li> 
+            <li'.$li['articulos'].'><a href="'.$cd.'pages/inventario/articulos.php"><i class="fa fa-circle-o"></i> Artículos</a></li>
+            <li'.$li['conversiones'].'><a href="'.$cd.'pages/inventario/conversiones.php"><i class="fa fa-circle-o"></i> Conversiones</a></li>
             <!-- <li class="treeview">
               <a href="#"><i class="fa fa-circle-o"></i> Nivel Uno
                 <span class="pull-right-container">
@@ -147,7 +296,7 @@
             </li> -->
           </ul>
         </li>
-        <li class="treeview">
+        <li class="'.$modulos['contabilidad'].'treeview">
           <a href="#">
             <i class="fa fa-share"></i> <span> Contabilidad</span>
             <span class="pull-right-container">
@@ -155,8 +304,8 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="pages/contabilidad/cierres_diarios.php"><i class="fa fa-circle-o"></i> Cierres Diarios</a></li> 
-            <li><a href="pages/contabilidad/reportes.php"><i class="fa fa-circle-o"></i> Reportes</a></li>
+            <li'.$li['cierres_diarios'].'><a href="'.$cd.'pages/contabilidad/cierres_diarios.php"><i class="fa fa-circle-o"></i> Cierres Diarios</a></li> 
+            <li'.$li['reportes'].'><a href="'.$cd.'pages/contabilidad/reportes.php"><i class="fa fa-circle-o"></i> Reportes</a></li>
             <!-- <li class="treeview">
               <a href="#"><i class="fa fa-circle-o"></i> Nivel Uno
                 <span class="pull-right-container">
@@ -180,7 +329,7 @@
             </li> -->
           </ul>
         </li>
-        <li class="treeview">
+        <li class="'.$modulos['configuraciones'].'treeview">
           <a href="#">
             <i class="fa fa-share"></i> <span> Configuraciones</span>
             <span class="pull-right-container">
@@ -188,7 +337,7 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="pages/configuraciones/perfil.php"><i class="fa fa-circle-o"></i> Perfil</a></li> 
+            <li'.$li['perfil'].'><a href="'.$cd.'pages/configuraciones/perfil.php"><i class="fa fa-circle-o"></i> Perfil</a></li> 
             <!-- <li class="treeview">
               <a href="#"><i class="fa fa-circle-o"></i> Nivel Uno
                 <span class="pull-right-container">
@@ -217,7 +366,7 @@
         <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Advertencia</span></a></li>
         <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Informacion</span></a></li>
       </ul>
-
-<?php
+      ';
+      unset($tree, $admin, $compras, $conf, $cont, $inv, $ventas, $li);
     }
 ?>
