74ab1e5bb7f6b416b47fbed1377b4e8943efe100
CVz <carlosdavidvasquez7@gmail.com>
2018-07-08 22:22:48 -0600

    Adicion y validaicion de los tipos de menus permitidos

--- inc/constructor.php
+++ inc/constructor.php
@@ -1,8 +1,42 @@
 <?php
-include ('menu.php');
+	include ('menus.php');
 
-function menu()	{
-	menuRoot();
-	
+	$principales = array('index', 'lockscreen');
+
+	function atras($fileName){
+		// if(in_array($fileName, $principales)){}
+		if ($fileName=='index'){
+			return '';
+		} else {
+			return '../../';
+		}
 	}
+
+	function menu($tipoUsuario, $fileName)	{
+		$cd = atras($filename);
+		switch ($tipoUsuario) {
+			case 'Superusuario':
+				menuRoot($cd, $fileName);
+				break;
+			case 'Administracion':
+				menuAdmin($cd, $fileName);
+				break;
+			case 'Compras':
+				menuCompras($cd, $fileName);
+				break;
+			case 'Contabilidad':
+				menuCont($cd, $fileName);
+				break;
+			case 'Inventario':
+				menuInv($cd, $fileName);
+				break;
+			case 'Ventas':
+				menuVentas($cd, $fileName);
+				break;
+			default:
+				# code...
+				break;
+		}
+	}
+
 ?>
