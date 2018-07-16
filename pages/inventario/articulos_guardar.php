7d3c232c94771d474c6570bb824b458c162c223e
Kunai11 <carlomeza11.cm@gmail.com>
2018-07-16 11:41:36 -0600

    Modulo articulos
    Archivo guardar

--- pages/inventario/articulos_guardar.php
+++ pages/inventario/articulos_guardar.php
@@ -4,38 +4,28 @@
 
         include ('../../inc/conexion.php');
         include ('../../inc/util.php');
-      var codigoArticulo = $("#codigo_articulo").val();
-      var descripcionArticulo= $("#descripcion_articulo").val();
-      var existencias= $("#existencias_articulos").val();
-      var existenciasMinimas= $("#existencias_minimas_articulos").val();
-      var precioArticulo= $("#precio_articulo").val();
-      var ganancia= $("#ganancia_articulos").val();
-      var estado = $('input[name="optionsRadios"]:checked').val();
-      var proveedor= $("#proveedor_articulor").val();
-      var ultimaCompra= $("#fecha_compra").val();
-      var ultimaVenta= $("#fecha_venta").val();
-      var categoria= $("#categoria_articulo").val();
+      
 
 		 $codigoArticulo = $_POST['codigo_articulo'];
 		 $descripcionArticulo = $_POST['descripcion_articulo'];
 		 $existencias = $_POST['estado'];
-		 $idEmpleado = $_POST['id_empleado'];
-		 $nombres = $_POST['nombres_empleado'];
-		 $apellido1 = $_POST['apellido_1'];
-		 $apellido2 = $_POST['apellido_2'];
-         $fechaNacimiento = fechaIngABD($_POST['fecha_nacimiento']);
-         $genero = $_POST['genero'];
-         $direccion = $_POST['direccion_empleado'];
-         $telefono = $_POST['telefono'];
-         $correo = $_POST['correo'];
+		 $existenciasMinimas = $_POST['existencias_minimas_articulos'];
+		 $precioArticulo = $_POST['precio_articulo'];
+		 $ganancia = $_POST['ganancia_articulos'];
+		 $estado = $_POST['input[name="optionsRadios"]:checked'];
+     $proveedor = $_POST['proveedor_articulo'];
+     $ultimaCompra = fechaIngABD($_POST['fecha_compra']);
+     $ultimaVenta = fechaIngABD($_POST['fecha_venta']);
+     $categoria = $_POST['categoria_articulo'];
+         
 
     //  echo $codigoEmpleado . ' <-> ' . $fechaIngreso . ' <-> ' . $estado . ' <-> ' . $idEmpleado . ' <-> ' . $nombres . ' <-> ' . $apellido1 .  ' <-> ' . $apellido2 . ' <-> ' . $fechaNacimiento . ' <-> ' . $genero . ' <-> ' . $direccion . ' <-> ' . $telefono . ' <-> ' . $correo;
 
-	$queryVerificar = mysqli_query($db, "SELECT COUNT(*) as Existe FROM empleados WHERE ID = '$idEmpleado'") or die (mysqli_error());
+	$queryVerificar = mysqli_query($db, "SELECT COUNT(*) as Existe FROM articulos WHERE Id_Articulo = '$codigoArticulo'") or die (mysqli_error());
     
 	$rowExiste=mysqli_fetch_array($queryVerificar);
 	if($rowExiste['Existe']==0){
-		$queryGuardar = mysqli_query($db, "INSERT INTO empleados (Codigo_Empleado, ID, Nombres, Apellido_1, Apellido_2, Fecha_Nacimiento, Fecha_Ingreso, Genero, Direccion, Telefono, Correo_Electronico, Estado) VALUES ('$codigoEmpleado', '$idEmpleado', '$nombres', '$apellido1', '$apellido2', '$fechaNacimiento', '$fechaIngreso', '$genero', '$direccion', '$telefono', '$correo', $estado) ") or die(mysqli_error());
+		$queryGuardar = mysqli_query($db, "INSERT INTO articulos (Id_Articulo, Descripcion, Existencias, Existencias_Minimas, Precio_Final, Porcentaje_Ganancia, Estado, Id_Proveedor, Fecha_Ultima_Compra, Fecha_Ultima_Venta, Id_Categoria) VALUES ('$codigoArticulo', '$descripcionArticulo', '$existencias', '$existenciasMinimas', '$precioArticulo', '$ganancia', '$estado', '$proveedor', '$ultimaCompra', '$ultimaVenta', '$categoria') ") or die(mysqli_error());
 		echo 'Guardado';
 	}
 	if ($rowExiste['Existe']==1) {
