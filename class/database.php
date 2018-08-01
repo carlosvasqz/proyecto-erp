669d26a8f118587ac676da6169e9265de05a4a9e
CVz <carlosdavidvasquez7@gmail.com>
2018-08-01 17:10:16 -0600

    Correcciones posibles en uso del constructor

--- class/database.php
+++ class/database.php
@@ -1,27 +1,30 @@
 <?php 
 class Database extends PDO{
 
-	public function __construct(){
-		try{
-			parent::__construct('mysql:host=localhost;dbname=proyecto_erp','root','');
-			parent::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		}catch (Exception $ex){
-			echo $ex . '<br>';
-			die('Error al conectar a la base de datos.');
+	// public function __construct(){
+	// 	try{
+	// 		parent::__construct('mysql:host=localhost;dbname=proyecto_erp','root','');
+	// 		parent::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// 	}catch (Exception $ex){
+	// 		echo $ex . '<br>';
+	// 		die('Error al conectar a la base de datos.');
+	// 	}
+	// }
+
+	public function __construct() {
+		$mysql_db='proyecto_erp';
+		$mysql_host='localhost';
+		$dsn = 'mysql:dbname=' . $mysql_db . ';host=' . $mysql_host;
+		$user = 'root';
+		$pw = '';
+		try {
+			parent::__construct($dsn, $user, $pw);
+			$this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		}
+		catch(PDOException $e) {
+			echo '<b>Connection failed:</b> ' . $e->getMessage();;
 		}
 	}
- }
-/* 
-public function __construct() {
-    $dsn = 'mysql:dbname=' . MYSQL_DB . ';host=' . MYSQL_HOST;
-    $user = MYSQL_USER;
-    $pw = MYSQL_PW;
-    try {
-        parent::__construct($dsn, $user, $pw);
-        $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch(PDOException $e) {
-        echo 'Connection failed: ' . $e->getMessage();;
-    }
+
 }
-*/
+?>
